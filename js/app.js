8ad50f5dea0a4e03fb5ab9a21f4089566fa8fbfc
talabh <talab.hussein@asu.edu>
2017-04-23 13:12:25 -0700

    added updates

--- js/app.js
+++ js/app.js
@@ -144,6 +144,28 @@ function saveTextAsPDF() {
     downloadLink.click();
 }
 
+function saveTextAsRTF() {
+    var textToWrite = document.getElementById('textArea').value;
+    var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/plain' });
+    var fileNameToSaveAs = "notes.rtf";
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null) {
+        // Chrome allows the link to be clicked without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    } else {
+        // Firefox requires the link to be added to the DOM before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+}
+
 function saveTextAsText() {
     var textToWrite = document.getElementById('textArea').value;
     var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/plain' });
